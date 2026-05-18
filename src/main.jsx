28e7d98625f19e6c1065158b8f9f76eeed30257f
asas23fd <asas23fd@users.noreply.github.com>
2026-05-18 17:46:33 -0300

    fix: correct BrowserRouter basename to /authshield-site

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/auth-dashboard">
+    <BrowserRouter basename="/authshield-site">
       <ThemeProvider>
         <AuthProvider>
           <App />
